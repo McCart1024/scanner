af192c1a9d570ad7476b5ec8d87bf4745ed98214
McCart1024 <laktouze@gmail.com>
2022-07-27 01:04:29 +0200

    calc 2.0

--- src/main/java/csomag/calculator.java
+++ src/main/java/csomag/calculator.java
@@ -9,37 +9,47 @@ public class calculator {
 
         Scanner scanner = new Scanner(System.in);
         int answer;
+        boolean bool=false;
 
         System.out.println("1. szam:");
         int firstNumber = scanner.nextInt();
         System.out.println("2. szam:");
         int secondNumber = scanner.nextInt();
-        System.out.println("Milyen muveletet vegeznel rajtuk? \n osszeadas: + \n kivonas: - \n szorzas: * \n osztas: /");
-        char operator = scanner.next().charAt(0);
 
-        switch (operator) {
-            case '+':
-                answer = firstNumber + secondNumber;
-                System.out.println(answer);
-                break;
-            case '-':
-                answer = firstNumber - secondNumber;
-                System.out.println(answer);
-                break;
-            case '/':
-                answer = firstNumber / secondNumber;
-                System.out.println(answer);
-                break;
-            case '*':
-                answer = firstNumber * secondNumber;
-                System.out.println(answer);
-                break;
+        do {
 
-            default:
-                System.out.println("Nem ertelmezheto muvelet.");
 
-        }
+            System.out.println("Milyen muveletet vegeznel rajtuk? \n osszeadas: + \n kivonas: - \n szorzas: * \n osztas: /");
+            char operator = scanner.next().charAt(0);
 
+            switch (operator) {
+                case '+':
+                    answer = firstNumber + secondNumber;
+                    System.out.println(answer);
+                    bool = true;
+                    break;
+                case '-':
+                    answer = firstNumber - secondNumber;
+                    System.out.println(answer);
+                    bool = true;
+                    break;
+                case '/':
+                    answer = firstNumber / secondNumber;
+                    System.out.println(answer);
+                    bool = true;
+                    break;
+                case '*':
+                    answer = firstNumber * secondNumber;
+                    System.out.println(answer);
+                    bool = true;
+                    break;
+
+                default:
+                    System.out.println("Nem ertelmezheto muvelet. Probald ujra.");
+            }
+
+
+        } while (bool==false);
 
     }
 }
